acc5ee361e51d3435a1c7660a7a47dde529d3c7c
aksinghal86 <aksinghal.aks@gmail.com>
2023-05-10 11:58:23 -0700

    exercise 1.3

--- exercises/part1/courseinfo/src/App.js
+++ exercises/part1/courseinfo/src/App.js
@@ -1,5 +1,5 @@
 // https://fullstackopen.com/en/part1/introduction_to_react
-// Exercises 1.1 and 1.2 
+// Exercises 1.3
 
 const Header = (props) => {
   return(
@@ -14,6 +14,7 @@ const Part = (props) => {
 }
 
 const Content = (props) => { 
+  // console.log(props)
   return(
     <div>
       <Part part={props.parts[0]} exercise={props.exercises[0]} />
@@ -30,21 +31,26 @@ const Total = (props) => {
 }
 const App = () => {
   const course = "Half Stack application development"
-  const part1 = "Fundamentals of React"
-  const exercises1 = 10
-  const part2 = "Using props to pass data"
-  const exercises2 = 7
-  const part3 = "State of a component"
-  const exercises3 = 14
+  const part1 = {
+    name: "Fundamentals of React", 
+    exercises: 10
+  }
+  const part2 = {
+    name: "Using props to pass data", 
+    exercises: 7
+  }
+  const part3 = {
+    name: "State of a component", 
+    exercises: 14
+  }
   
   const parts = [part1, part2, part3]
-  const exercises = [exercises1, exercises2, exercises3]
 
   return (
     <div> 
       <Header course={course} />
-      <Content parts={parts} exercises={exercises} />
-      <Total total={exercises1 + exercises2 + exercises3}/>
+      <Content parts={[part1.name, part2.name, part3.name]} exercises = {[part1.exercises, part2.exercises, part3.exercises]} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises}/>
     </div>
   )
 }
