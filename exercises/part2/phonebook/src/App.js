827e8c8676cbf61c690ee3ef7584b095b25447b6
aksinghal86 <aksinghal.aks@gmail.com>
2023-05-17 16:44:37 -0700

    excercise 2.11

--- exercises/part2/phonebook/src/App.js
+++ exercises/part2/phonebook/src/App.js
@@ -1,21 +1,26 @@
 // https://fullstackopen.com/en/part2/forms
-// Exercises 2.6, 2.7, 2.8, 2.9, 2.10
+// Exercises 2.6, 2.7, 2.8, 2.9, 2.10, 2.11
 
-import { useState } from 'react';
-import Filter from './components/Filter';
-import Persons from './components/Persons';
-import PersonForm from './components/PersonForm';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Filter from './components/Filter'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [showPerson, setShowPerson] = useState(persons)
+  const [showPerson, setShowPerson] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+        setShowPerson(response.data)
+      })
+  }, [])
 
   const addName = (event) => {
     event.preventDefault()
