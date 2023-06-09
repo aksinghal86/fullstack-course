c313a2658def9af38919b28eb4aa03daa5e027bc
Ankur Singhal <aksinghal.aks@gmail.com>
2023-06-09 10:44:49 -0700

    updated notes backend

--- notes/part3/index.js
+++ notes/part3/index.js
@@ -1,9 +1,8 @@
 require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
-
-const Note = require('./models/note')
 const app = express()
+const cors = require('cors')
+const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
   console.log('Method: ', request.method)
@@ -17,17 +16,13 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "Unknown endpoint."} )
 }
 
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 app.use(requestLogger)
 app.use(express.static('build')) 
 
 let notes = [
 ]
-
-app.get('/', (request, response) => { 
-  response.send('<h1>Hello world</h1>')
-})
 
 app.get('/api/notes', (request, response) => { 
   Note.find({}).then(notes => {
